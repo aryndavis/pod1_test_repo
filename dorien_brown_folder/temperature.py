6fcab35e7469ce79969dda5f89185ba2ab63252e
deedot718 <dorien.brown@yahoo.com>
2021-02-01 20:40:35 -0500

    temperature converse todays challenge

---
+++ dorien_brown_folder/temperature.py
@@ -0,0 +1,12 @@
+celsius_100 =(100-32)*5/9
+print (celsius_100)
+print('This value is a float bc it has decimal points.')
+
+celsius_0 =(0-32)*5/9
+print (celsius_0)
+print('This value is a float bc it has decimal points')
+
+
+fahrenheit_5 = (5*9/5)+32
+print (fahrenheit_5)
+print ('This is an integerbc there is no non zeros value in the decimal place')
