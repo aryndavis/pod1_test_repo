9f2cb50136f0b13b81de3a37e355f1e36a02e9cd
gmartinez948 <gmartinez948@outlook.com>
2021-02-02 00:30:50 -0500

    temperature.py

--- guillermo_martinez_folder/temperature.py
+++ guillermo_martinez_folder/temperature.py
@@ -1,11 +1,11 @@
-#Making a temperature converter
+'''Making a temperature converter'''
 
-#converting 100 degrees farenheight to celcius
+'''converting 100 degrees farenheight to celcius'''
 celcius_100 = (100-32)*(5/9)
 print(celcius_100)
-# The answer is a float because it has decimals.
+'''The answer is a float because it has decimals'''
 
-#converting 0 degrees F to celcius
+'''converting 0 degrees F to celcius'''
 celcius_0 = (0-32)*(5/9)
 print(celcius_0) 
 
@@ -14,9 +14,9 @@ print(celcius_0)
  '''
 
 print((34.2-32) * (5/9))
-#converting back celcius to farenheit ..
+'''converting back celcius to farenheit '''
 print((9/5 * 5 )+ 32)
-# trying to find out what's hotter. 30.2 C or 85.1 F
+''' trying to find out what's hotter. 30.2 C or 85.1 F'''
 print((85.1-32)*(5/9)
 
-# 30.2 C is hotter than 85.1 F
+''' 30.2 C is hotter than 85.1 F'''
