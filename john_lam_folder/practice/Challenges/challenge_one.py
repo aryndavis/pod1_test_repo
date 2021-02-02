6dee6a86afd309ce00dbef64663e947bec2fe3ea
JohnLam916 <johnvenhlam@gmail.com>
2021-02-01 16:59:32 -0800

    My temperature converter

---
+++ john_lam_folder/practice/Challenges/challenge_one.py
@@ -0,0 +1,5 @@
+temperature = input("What temperature do you want to convert to farenheit?")
+temperature_num = float(temperature)
+temperature_converter = temperature_num * 1.8 + 32
+
+print(temperature_converter)
