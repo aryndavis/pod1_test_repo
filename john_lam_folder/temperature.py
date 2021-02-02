394109c907d41366b1edcceaf7e63f53bdb6edd4
JohnLam916 <johnvenhlam@gmail.com>
2021-02-01 17:17:54 -0800

    My Temperature Conversions

--- john_lam_folder/temperature.py
+++ john_lam_folder/temperature.py
@@ -1,5 +1,15 @@
-temperature = input("What temperature do you want to convert to farenheit?")
-temperature_num = float(temperature)
-temperature_converter = temperature_num * 1.8 + 32
+celsius_100 = (100-32) * 5/9 
+print(celsius_100)
 
-print(temperature_converter)
+celsius_0 = (0-32) * 5/9
+print(celsius_0)
+
+print(int(((34.2 -32) * 5/9)))
+
+celsius_5 = (((5 *1.8) + 32))
+print(celsius_5)
+
+celsius_32 = (((32 *1.8) + 32))
+print(celsius_32)
+
+# 32 degree celsius is hotter than 85.1 degrees fahrenheit
