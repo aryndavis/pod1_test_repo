778c543b1675fe1ccb4a55f21800a2bfdae586e5
Aryn Davis <arynlynn1@gmail.com>
2021-02-02 18:50:54 -0600

    adding temp challenge

---
+++ example_student_folder/temperature.py
@@ -0,0 +1,23 @@
+# Question 1
+celsius_100 = (100-32)*(5/9)
+
+print(celsius_100)
+
+print('This value is a float bc it has decimal points.')
+
+# Question 2
+celsius_0 = (0-32)*(5/9)
+
+print(celsius_0)
+
+print('This value is a float bc it has decimal points.')
+
+# Question 3
+print( ((34.2-32)*5/9)  )
+
+# Question 4
+fahrenheit_5 = (5*9/5)+32
+
+print(fahrenheit_5)
+
+print("This number is an integer because there no non zeros values in the decimal places")
