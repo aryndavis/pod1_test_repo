ee7c34c7077bcde4ca84041e2c571c8f6bf85e36
JohnLam916 <johnvenhlam@gmail.com>
2021-02-23 21:41:19 -0800

    Playlist Challenge complete

--- john_lam_folder/playlist_challenge.py
+++ john_lam_folder/playlist_challenge.py
@@ -5,13 +5,16 @@
 
 # 1 Import all the functions in playlist_functions.py
 
+from playlist_function import *
+import numpy as np
+
 # This code initializes your playlist as an empty list. no songs in it yet!
 my_playlist = []
-
 
 # 2 Check what is in your playlist using the display_playlist() function
 # HINT: the display_playlist() function in playlist_functions.py to figure out how to use it
 print('Question 2')
+display_playlist(my_playlist)
 
 # 3 Add a song to my_playlist using the add_song() function
 # The song that you add should be a dictionary, with the following key-value pairs
@@ -21,20 +24,35 @@ print('Question 2')
 '''
 example_song = {'artist': 'Lauryn Hill', 'title': 'Everything Is Everything'}
 '''
+new_song = {'artist':'Johnny Cash', 
+               'title': 'hurt'} 
 
+add_song(my_playlist, new_song)
 
 # 4 Check that you've added the song by running the display_playlist() function again
 print('Question 4')
 
+display_playlist(my_playlist)
+
 # 5 Add 2 more songs to my_playlist, then display it again using the display_playlist() function
 print('Question 5')
 
+new_song1 = {'artist' : 'Lauren Hill',
+             'title' : 'Everything is everything'}
+             
+new_song2 = {'artist': 'NF', 
+             'title': 'lie'}
+
+add_song(my_playlist, new_song1)
+add_song(my_playlist, new_song2)
+display_playlist(my_playlist)
 
 # 6 In playlist_functions.py, define a function called get_playlist_length()
 # See playlist_functions.py for details on how to define this function
 # THEN, call that function in this script to get the length of my_playlist
 print('Question 6')
 
+print(get_playlist_length(my_playlist))
 
 # 7 At the top of this script, import numpy using the usual alias
 
@@ -44,8 +62,12 @@ print('Question 6')
 print('Question 8')
 monthly_plays = [127030, 274920, 232453, 98278, 500301, 235462]
 
+print(np.mean(monthly_plays))
+
 
 # BONUS In playlist_functions.py, define a new function called play_track()
 # See playlist_functions.py for details on how to define this function
 # Then play a few tracks, and run display_playlist() again to make sure it works
 print('BONUS')
+
+print(play_track(my_playlist, 0))
